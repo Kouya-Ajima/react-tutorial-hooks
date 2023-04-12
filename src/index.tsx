9356ca4a928b2afc96f2c81b1ba8a21fea737bb2
koya <azumabass@gmail.com>
2023-04-12 14:24:45 +0900

    CSSファイルを追加(#2)

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Square from './Square';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <div></div>
+        <Square />
     </React.StrictMode>
 );
