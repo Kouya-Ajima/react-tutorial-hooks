cfbe8ef2d44b87689a7793719dffebdc24f5d623
koya <azumabass@gmail.com>
2023-04-12 14:04:38 +0900

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <React.StrictMode>
+        <div></div>
+    </React.StrictMode>
+);
