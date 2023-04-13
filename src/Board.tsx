e202118d9c0dd2e59375a8130059b58536c88b14
koya <azumabass@gmail.com>
2023-04-13 11:32:51 +0900

    Clockを実装, ゲーム履歴を作成(#6)

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,47 +1,40 @@
 /** @format */
 import Square from './Square';
-import { useState } from 'react';
 import { SquareType } from './Game';
 import { calculateWinner } from './CalculateWinner';
 
-export default function Board() {
-    /**
-     * useState
-     *  -> Squareの現在の値をstateに保存し、Squareがクリックされたときにそれを変更する
-     */
-    // 配列を初期化
-    const squareDefault: SquareType[] = Array(9).fill(null);
-    // OX の配列 squares に初期値を代入、＆ 関数を宣言
-    const [squares, setSquares] = useState(squareDefault);
-    // OX を反転 xIsNext に初期値を代入、＆ 関数を宣言
-    const [xIsNext, setXIsNext] = useState(true);
+type BoardProps = {
+    xIsNext: boolean;
+    squares: SquareType[];
+    // TS では、引数にも型をつける。
+    onPlay: (nextSquares: SquareType[]) => void;
+};
 
+export default function Board(props: BoardProps) {
     /** click Buton -> writing 'X'  */
     function handleClick(i: number) {
         // 配列の初期値はNull → 値があれば Return する。
-        if (calculateWinner(squares) || squares[i]) return;
-        // 配列をコピー
-        const nextSquares = squares.slice();
-        // 取得した配列の０番目を変更
-        if (xIsNext) {
+        if (calculateWinner(props.squares) || props.squares[i]) return;
+        // 配列をコピー → OX の配列が格納されている。
+        const nextSquares = props.squares.slice();
+        // 取得した配列の０番目を変更 → 新たなOX を配列に記入する
+        if (props.xIsNext) {
             nextSquares[i] = 'X';
         } else {
             nextSquares[i] = 'O';
         }
-        // 配列を上書きする
-        setSquares(nextSquares);
-        // True を反転させて上書きする
-        setXIsNext(!xIsNext);
+        // 履歴に、新しく OX が書かれた 配列を追加する ＋ xIsNext を反転
+        props.onPlay(nextSquares);
     }
 
     // handleClick で変更があった時点で再レンダリングされる。
     //  → この記載でも、都度更新される。
-    const winner = calculateWinner(squares);
+    const winner = calculateWinner(props.squares);
     let status;
     if (winner) {
         status = 'Winner: ' + winner;
     } else {
-        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+        status = 'Next player: ' + (props.xIsNext ? 'X' : 'O');
     }
 
     return (
@@ -49,43 +42,43 @@ export default function Board() {
             <div className='status'>{status}</div>
             <div className='board-row'>
                 <Square
-                    value={squares[0]}
+                    value={props.squares[0]}
                     onSquareClick={() => handleClick(0)}
                 />
                 <Square
-                    value={squares[1]}
+                    value={props.squares[1]}
                     onSquareClick={() => handleClick(1)}
                 />
                 <Square
-                    value={squares[2]}
+                    value={props.squares[2]}
                     onSquareClick={() => handleClick(2)}
                 />
             </div>
             <div className='board-row'>
                 <Square
-                    value={squares[3]}
+                    value={props.squares[3]}
                     onSquareClick={() => handleClick(3)}
                 />
                 <Square
-                    value={squares[4]}
+                    value={props.squares[4]}
                     onSquareClick={() => handleClick(4)}
                 />
                 <Square
-                    value={squares[5]}
+                    value={props.squares[5]}
                     onSquareClick={() => handleClick(5)}
                 />
             </div>
             <div className='board-row'>
                 <Square
-                    value={squares[6]}
+                    value={props.squares[6]}
                     onSquareClick={() => handleClick(6)}
                 />
                 <Square
-                    value={squares[7]}
+                    value={props.squares[7]}
                     onSquareClick={() => handleClick(7)}
                 />
                 <Square
-                    value={squares[8]}
+                    value={props.squares[8]}
                     onSquareClick={() => handleClick(8)}
                 />
             </div>
--- src/Game.tsx
+++ src/Game.tsx
@@ -4,28 +4,46 @@ import Board from './Board';
 import Clock from './Clock';
 
 export type SquareType = 'O' | 'X' | null;
+/** Squares 配列 の配列。 History ＝ [Squares, Squares, Squares] */
+type HistoryType = SquareType[];
 
 /**
  * @notice
  * コンポーネントはファイルごとに作成するのが理想。
  *      →default をつけて{} なしでインポートする。
+ * useState
+ *      -> Squareの現在の値をstateに保存し、Squareがクリックされたときにそれを変更する
  * @returns
  */
 export default function Game() {
     // 配列を初期化
-    const historyDefault: SquareType[] = Array(9).fill(null);
+    const historyDefault: HistoryType[] = Array(9).fill(null);
     // OX を反転 xIsNext に初期値を代入、＆ 関数を宣言
-    const [xIsNext, setXIsNext] = useState(true);
-    const [history, setHistory] = useState(historyDefault);
+    const [xIsNext, setXIsNext] = useState<boolean>(true);
+    // Sqares の配列 history に初期値を代入、＆ 関数を宣言
+    const [history, setHistory] = useState<HistoryType[]>(historyDefault);
+    // 現在の履歴を取得する。 → 配列が0から始まるので、履歴の長さ −１
     const currentSquares = history[history.length - 1];
+
+    /** ボタン押下後、履歴を更新して OX をトグル */
+    function handlePlay(nextSquares: SquareType[]) {
+        // history の全履歴 ＋ 新しい履歴（Board から渡されたもの）
+        setHistory([...history, nextSquares]);
+        // O X を トグル
+        setXIsNext(!xIsNext);
+    }
 
     return (
         <>
             <Clock />
             <div className='game'>
                 <div className='game-board'>
-                    {/* <div className='status'>{status}</div> */}
-                    <Board />
+                    <Board
+                        xIsNext={xIsNext}
+                        // 現在の履歴を渡す
+                        squares={currentSquares}
+                        onPlay={handlePlay}
+                    />
                 </div>
                 <div className='game-info'>
                     <ol>{/*TODO*/}</ol>
